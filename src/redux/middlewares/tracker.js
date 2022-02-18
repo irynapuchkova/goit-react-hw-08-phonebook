7a17b3ba7dbb3e1df29efb8723a337070dfe86a1
Iryna Puchkova <irynapuchkova18@gmail.com>
2022-02-18 15:50:13 +0200

    delete middleware

--- src/redux/middlewares/tracker.js
+++
@@ -1,9 +0,0 @@
-const trackAction = (type) => {
-  console.log("tracker.type => ", type);
-};
-
-export const tracker = (store) => (next) => (action) => {
-  trackAction(action.type);
-
-  return next(action);
-};
